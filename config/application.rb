5fba5cf83feec0290e9713630f06015785ee04d2
Fatima Tahir <fatimatahir353@gmail.com>
2025-02-11 14:44:25 +0200

    remove default config

--- config/application.rb
+++ config/application.rb
@@ -7,10 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Rubyric
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-    
+  class Application < Rails::Application    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
